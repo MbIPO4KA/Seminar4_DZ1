362491a2afca46c7056eef85e3891ab86213e6eb
MbIPO4KA <63498319+MbIPO4KA@users.noreply.github.com>
2023-02-24 14:39:04 +0500

    Остановка. Проверка программного обеспечения

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Write("Введите число А: ");
+ int numberA = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите число B: ");
+ int numberB = Convert.ToInt32(Console.ReadLine());
+
+ int Begree(int numberA, int numberB);
+ {
+    int result = (numberA*numberA) * numberB;
+    return result;
+ }
+
+ Console.WriteLine(Begree(numberA, numberB));
